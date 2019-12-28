b86e41a33cf032a26ed5e98d2ae48a615c41dcb8
Colin Chavez <metalwhale.mw@gmail.com>
2019-12-27 21:19:34 -0600

    Add Source Files

---
+++ js-basics-mosh/index.js
@@ -0,0 +1,12 @@
+let name = 'Mosh'; //proper way in ES6
+console.log(name);
+// by default variables are undefined
+//Rules for naming variables-
+// Cannot be a reserved keyword.
+// Should be meaniingul
+// Cannot start with a number
+// Cannot contain a space or hyphen
+// Case - Sensitive
+
+let firstName = 'Mosh'; //camel notation, first letter is always lowercased
+let lastName = 'Hamedani';
