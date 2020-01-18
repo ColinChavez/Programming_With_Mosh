6eb3c9eedf9aad7764cdb29d397a0cf7666252da
Colin-Chavez <c.chavez3451@gmail.com>
2020-01-18 07:30:20 -0600

    Updated Course Notes

--- js-basics-mosh/notes.js
+++ js-basics-mosh/notes.js
@@ -445,7 +445,8 @@ const person = {
 
 for (let key in person)
     console.log(key, person[key]);
-// the key variable in the loop will hold one of the properties in this person object
+// the key variable in the loop will hold one of the p
+roperties in this person object
 
 //used to display the property of each key, 
 // dot notation
@@ -537,4 +538,28 @@ function fizzBuzz(input) {
 
 
     return input;
+}
+
+// 4. Checkspeed
+// Speed Limit = 40
+// every 5  over 70 they get a point
+//Math.floor(1.3)
+// 12 points suspended
+
+checkSpeed(109);
+
+function checkSpeed(speed) {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if (speed < speedLimit + kmPerPoint) {
+        console.log('Ok');
+        return;
+    }
+    
+    const points = Math.floor((speed - speedLimit) / kmPerPoint);
+    if (points >= 12)
+     console.log('License Suspended');
+    else
+     console.log('Points', points); 
 }
--- js_basics/index.js
+++ js_basics/index.js
@@ -1,27 +1,24 @@
 //Excercises
 
-//Divisible by 3 == Fizz
-//Divisible by 5 == Buzz
-// Divisible by both 3 and 5 == FizzBuzz
-// Not divisible by 3 or 5 == input
-//Not a number == 'Not a Number'
+// Speed Limit = 40
+// every 5  over 70 they get a point
+//Math.floor(1.3)
+// 12 points suspended
 
-const output = fizzBuzz(false);
-console.log (output);
+checkSpeed(109);
 
-function fizzBuzz(input) {
-    if (typeof input !== 'number')
-    return NaN;
+function checkSpeed(speed) {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if (speed < speedLimit + kmPerPoint) {
+        console.log('Ok');
+        return;
+    }
     
-    if ((input % 3 === 0) && (input % 5 === 0))
-    return 'FizzBuzz';
-    
-    if (input % 3 === 0)
-    return 'Fizz';
-
-    if (input % 5 === 0)
-    return 'Buzz';
-
-
-    return input;
+    const points = Math.floor((speed - speedLimit) / kmPerPoint);
+    if (points >= 12)
+     console.log('License Suspended');
+    else
+     console.log('Points', points); 
 }
